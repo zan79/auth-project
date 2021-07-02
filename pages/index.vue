08fa535150cfe2bc4c1d7f23d169bc72e8775c4c
ZAN <zanders.lsp@gmail.com>
2021-07-02 21:55:57 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="container mt-5">
+      <h1 class="text-warning text-center pt-5">Welcome to the page</h1>
+
+      <h3 class="text-light text-center mt-5" v-if="$auth.loggedIn">
+      {{$auth.user.name}}
+      </h3>
+
+      <h2 class="text-secondary text-center mt-5">
+        Have a nice day =)
+      </h2>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false
+}
+</script>
+
+<style>
+body{
+  background-color: #212121;
+}
+</style>
